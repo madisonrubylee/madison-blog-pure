f09a76f31866ed58b4962555ca827be66df8db0e
madisonrubylee <lamantciel@naver.com>
2022-03-08 23:34:01 +0900

    css 수정, 메뉴 추가

--- components/contents/content.tsx
+++ components/contents/content.tsx
@@ -3,9 +3,22 @@ import styles from './content.module.css'
 
 const Content = () => {
     return (
-        <>
+        <>  
             <div className={styles.container}>
                     
+            </div>
+            <div className={styles.tabs}>
+                <ul className={styles.tabList}>
+                    <li className={styles.tabMenu}>
+                        <button className={styles.btn}>home</button>
+                    </li>
+                    <li className={styles.tabMenu}>
+                        <button className={styles.btn}>skills</button>
+                    </li>
+                    <li className={styles.tabMenu}>
+                        <button className={styles.btn}>experience</button>
+                    </li>
+                </ul>
             </div>
         </>
     )
