4d3370ac3876af2ebe85b5a5cfdd49b1d8719074
madisonrubylee <lamantciel@naver.com>
2022-03-10 22:53:54 +0900

    add cloud

--- components/menu_home/home.tsx
+++ components/menu_home/home.tsx
@@ -5,7 +5,7 @@ import styles from './home.module.css'
 const Home = () => {
     return (
         <>
-            <h2>Madison's Profile</h2>
+            <h2 className={styles.motto}>인생은 산넘어 산</h2>
             {/* <hr className={styles.divide}></hr> */}
             <div className={styles.container}>
                 {/* <div>타임라인</div> */}
--- components/timeline/timeline.tsx
+++ components/timeline/timeline.tsx
@@ -3,7 +3,7 @@ import styles from './timeline.module.css'
 
 const Timeline = () =>{ 
     return (
-        <>
+        <div>
             <div className={styles.mountain}>
                 <div className={styles.top}>
                 <div className={styles.one}></div>
@@ -11,8 +11,22 @@ const Timeline = () =>{
                 <div className={styles.three}></div>
                 </div>
             </div>
+            <div className={styles.secondMountain}>
+                <div className={styles.top}>
+                <div className={styles.one}></div>
+                <div className={styles.two}></div>
+                <div className={styles.three}></div>
+                </div>
+            </div>
+            <div className={styles.thirdMountain}>
+                <div className={styles.top}>
+                <div className={styles.one}></div>
+                <div className={styles.two}></div>
+                <div className={styles.three}></div>
+                </div>
+            </div>
         <div className={styles.cloud}></div>
-        </>
+        </div>
     )
 }
 
