b337225e9e8859f1adf1a6c3b9dca719156797a5
madisonrubylee <lamantciel@naver.com>
2022-03-15 23:52:34 +0900

    말풍선

--- components/skills/skills.tsx
+++ components/skills/skills.tsx
@@ -8,11 +8,12 @@ const Skills = () => {
                 <p className={styles.typefont}>Front-end</p>
                 <div className={styles.wrapper}> 
                     <ul className={styles.line}>
-                        <li className={styles.dot}>2019</li>
+                        <li className={`${styles.dot} ${styles.first}`}>2019</li>
                         <li className={styles.dot}>2020</li>
                         <li className={styles.dot}>2021</li>
                         <li className={`${styles.dot} ${styles.active}`}>2022</li>
                     </ul>
+                    <div className={styles.skill}>JavaScript, HTML, CSS</div>
                 </div>
             </div>
         </>
