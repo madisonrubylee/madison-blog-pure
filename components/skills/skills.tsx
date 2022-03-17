d0bd0168e1d5f58e3434d688b5f06aa05aa340f6
madisonrubylee <lamantciel@naver.com>
2022-03-17 23:56:55 +0900

    말풍선 조절 , 토글 생성

--- components/skills/skills.tsx
+++ components/skills/skills.tsx
@@ -1,15 +1,29 @@
-import React from 'react';
+import React, {useState}from 'react';
 import styles from './skills.module.css'
 
 const Skills = () => {
+    const [isChecked, setIsChecked] = useState(false) 
+
+    const handleToggle = (e: React.ChangeEvent<HTMLInputElement>) => {
+        if(isChecked) {
+            setIsChecked(false)  
+        } else {
+            setIsChecked(true)
+        }
+    }
+
     return (
         <>
             <div className={styles.type}>
                 <p className={styles.typefont}>Front-end</p>
+                <label className={styles.switch}>
+                    <input type={styles.checkbox} checked={isChecked} onChange={(e) => handleToggle(e)}/>
+                    <span className={styles.slider}></span>
+                </label>
                 <div className={styles.wrapper}> 
                     <ul className={styles.line}>
                         <li className={`${styles.dot} ${styles.first}`}>2019
-                            <div className={styles.skill}>JavaScript, HTML, CSS</div>
+                            <div className={styles.skill}>JavaScript, jQuery, HTML, CSS</div>
                         </li>
                         <li className={styles.dot}>2020</li>
                         <li className={styles.dot}>2021</li>
