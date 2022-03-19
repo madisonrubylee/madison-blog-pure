3cb8a49ac2c06a310384d38f29bb9d233e05ee4e
madisonrubylee <lamantciel@naver.com>
2022-03-19 09:41:56 +0900

    toggle color change:

--- components/skills/skills.tsx
+++ components/skills/skills.tsx
@@ -3,24 +3,31 @@ import styles from './skills.module.css'
 
 const Skills = () => {
     const [isChecked, setIsChecked] = useState(true) 
+    const [skill, setSkill] = useState('FE')
 
-    const handleToggle = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const handleToggle = (e: React.MouseEvent<HTMLInputElement>) => {
         if(isChecked) {
             setIsChecked(false)  
+            setSkill('BE')
         } else {
             setIsChecked(true)
+            setSkill('FE')
         }
     }
 
+    const skillDesc = skill === 'FE' ? 'Front-end' : 'Back-end'
     return (
         <>
-            <div className={styles.type}>
-                <p className={styles.typefont}>Front-end</p>
+            <div className={`${styles.type} ${skill === 'BE' && styles.be} `}>
+                <div className={styles.skillToggle} > 
+                    <p className={styles.typefont}>{skillDesc}</p>
                     <label className={styles.switch}>
                         <input type='checkbox' defaultChecked={isChecked || true} onClick={(e) => handleToggle(e)}/>
                         <span className={styles.slider}></span>
                     </label>
-                <div className={styles.wrapper}> 
+                </div>
+            </div>
+            <div className={styles.wrapper}> 
                     <ul className={styles.line}>
                         <li className={`${styles.dot} ${styles.first}`}>2019
                             <div className={styles.skill}>JavaScript, jQuery, HTML, CSS</div>
@@ -30,7 +37,6 @@ const Skills = () => {
                         <li className={`${styles.dot} ${styles.active}`}>2022</li>
                     </ul>
                 </div>
-            </div>
         </>
     )
 }
